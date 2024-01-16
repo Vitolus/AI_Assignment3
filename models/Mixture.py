e19ffbe201f58105311fdcee046150e02761559c
Vitolus <vito.davide00@gmail.com>
2024-01-16 18:06:55 +0100

    update gaussian, TODO time

--- models/Mixture.py
+++ models/Mixture.py
@@ -2,7 +2,8 @@ from sklearn.datasets import fetch_openml
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
 from atom import ATOMClassifier
-from itertools import combinations
+import cupy as cp
+from cupyx.scipy.spatial.distance import pdist
 
 
 class Mixture:
@@ -10,7 +11,7 @@ class Mixture:
         self.X, self.y = fetch_openml('mnist_784', version=1, return_X_y=True)  # load data
         self.y = self.y.astype(int)  # convert string to int
         self.X = self.X / 255.  # normalize data
-        self.model = ATOMClassifier(self.X, self.y, test_size=2000, n_jobs=-1, n_rows=10000,
+        self.model = ATOMClassifier(self.X, self.y, test_size=10000, n_jobs=-1, n_rows=70000,
                                     device="clu", engine="sklearn", verbose=2, random_state=1)
         self.X_train = self.model.X_train.to_numpy()
         self.X_test = self.model.X_test.to_numpy()
@@ -22,25 +23,19 @@ class Mixture:
         # Iterate over different PCA dimensions
         for n_components in range(2, 203, 10):
             pca = PCA(n_components=n_components)
-            X_train_pca = pca.fit_transform(self.X_train)
-            X_test_pca = pca.transform(self.X_test)
             best_params.append({'components': n_components, 'clusters': 0, 'rand_index': 0.0})
             # Iterate over different number of clusters
             for n_clusters in range(5, 16):
                 gm = GaussianMixture(n_components=n_clusters, covariance_type='diag')
-                gm.fit(X_train_pca)
-                y_pred = gm.predict(X_test_pca)
+                gm.fit(pca.fit_transform(self.X_train))
+                y_pred = cp.asarray(gm.predict(pca.transform(self.X_test)))
                 # Calculate the Rand index
-                n = len(self.y_test)
-                a = 0
-                b = 0
-                for i, j in combinations(range(n), 2):
-                    if self.y_test[i] == self.y_test[j] and y_pred[i] == y_pred[j]:
-                        a += 1
-                    elif self.y_test[i] != self.y_test[j] and y_pred[i] != y_pred[j]:
-                        b += 1
-                    print(f'Progress: Components: {n_components}, Clusters: {n_clusters}, {i}/{n}, {j}/{n}', end='\r')
-                r = 2 * (a + b) / (n * (n - 1))
+                y_test = cp.asarray(self.y_test)
+                n = len(y_test)
+                # Calculate pairwise equality for y_test and y_pred
+                eq_test = pdist(y_test[:, None], metric='euclidean') == 0
+                eq_pred = pdist(y_pred[:, None], metric='euclidean') == 0
+                r = 2 * (cp.sum(eq_test & eq_pred) + cp.sum(~eq_test & ~eq_pred)) / (n * (n - 1))
                 # Save the best parameters
                 if r > best_params[-1]['rand_index']:
                     best_params[-1]['clusters'] = n_clusters
