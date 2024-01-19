fbff49cb74a9fcac82b35bdb34d078d3b95a4664
Vitolus <vito.davide00@gmail.com>
2024-01-19 13:55:12 +0100

    add normalized cut, TODO time

--- models/Cut.py
+++ models/Cut.py
@@ -1,8 +1,16 @@
 from models.Classifier import Classifier
 from sklearn.decomposition import PCA
-from sklearn.mixture import GaussianMixture
+from sklearn.cluster import SpectralClustering
 import cupy as cp
 from cupyx.scipy.spatial.distance import pdist
+from sklearn.metrics.pairwise import rbf_kernel
+
+
+def _predict(labels, X_train, X_test, k=10):
+    similarity = rbf_kernel(X_test, X_train)
+    k_idx = cp.argsort(similarity, axis=1)[:, -k:]
+    k_labels = labels[k_idx]
+    return cp.asarray([cp.argmax(cp.bincount(label)) for label in k_labels])
 
 
 class Mixture(Classifier):
@@ -14,12 +22,13 @@ class Mixture(Classifier):
         # Iterate over different PCA dimensions
         for n_components in range(2, 203, 10):
             pca = PCA(n_components=n_components)
+            X_train_pca = pca.fit_transform(self.X_train)
             best_params.append({'components': n_components, 'clusters': 0, 'rand_index': 0.0})
             # Iterate over different number of clusters
             for n_clusters in range(5, 16):
-                gm = GaussianMixture(n_components=n_clusters, covariance_type='diag', random_state=1, verbose=2)
-                gm.fit(pca.fit_transform(self.X_train))
-                y_pred = cp.asarray(gm.predict(pca.transform(self.X_test)))
+                sc = SpectralClustering(n_clusters=n_clusters, n_jobs=-1, random_state=1, verbose=2)
+                sc.fit(X_train_pca)
+                y_pred = _predict(sc.labels_, X_train_pca, pca.transform(self.X_test))
                 # Calculate the Rand index
                 y_test = cp.asarray(self.y_test)
                 n = len(y_test)
@@ -33,3 +42,4 @@ class Mixture(Classifier):
                     best_params[-1]['rand_index'] = r
             # Print the best parameters
             print(f'Best parameters for {n_components} components: {best_params[-1]}')
+
